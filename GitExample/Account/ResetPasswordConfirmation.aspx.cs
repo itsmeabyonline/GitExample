078d5ec0c8d672907b8ba7dec7d29d7b741dff86
Aby Abraham <itsmeaby@hotmail.com>
2020-07-10 21:25:22 +0300

    My first commit !!

---
+++ GitExample/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace GitExample.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ GitExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitExample.Global" Language="C#" %>
